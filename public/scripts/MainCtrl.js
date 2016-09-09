1968ac79347b7fccc4f122edea90cf4fb58189a5
Meracoid <jcreech3@student.gsu.edu>
2016-09-09 19:55:55 -0400

    Start - Added more to email directive and added git ignore

--- public/scripts/MainCtrl.js
+++ public/scripts/MainCtrl.js
@@ -1,7 +1,9 @@
 define(['angular'], function(angular){
-  angular.module('controllers).controller(
+  angular.module('controllers').controller(
     "MainCtrl",
     ["$scope", "$rootScope",
     function($scope, $rootScope){
-  }]);
+
+    }
+  ]);
 });
--- public/scripts/email.js
+++ public/scripts/email.js
@@ -1,8 +1,11 @@
+//  <email></email>
+//  created by John Wayne 'meracoid' Creech
+
 define(['angular'], function(angular){
   angular.module('main').directive('email',['email', function(email){
     return{
       restrict:'E',
-      templateUrl: '',
+      templateUrl: 'public/views/email.html',
       scope:{
       },
       link: function(){
