575694e7a8692bf003bd5fae2985a2358c8b9bc0
Meracoid <jcreech3@student.gsu.edu>
2016-09-12 12:35:34 -0400

    Deleted public folder

--- public/scripts/MainCtrl.js
+++
@@ -1,9 +0,0 @@
-define(['angular'], function(angular){
-  angular.module('controllers').controller(
-    "MainCtrl",
-    ["$scope", "$rootScope",
-    function($scope, $rootScope){
-
-    }
-  ]);
-});
